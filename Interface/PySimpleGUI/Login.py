9e7c39270faf1ce73e4ca4e210e696ce4e07b05a
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-11-23 15:29:55 -0300

    Update Login.py

--- Interface/PySimpleGUI/Login.py
+++ Interface/PySimpleGUI/Login.py
@@ -1,22 +1,31 @@
 import PySimpleGUI as sg
-#editando o tema
-sg.theme ('black')
-
-#criando o formato
+#criand a estrutura da tela
 layout=[
-	[sg.Text("Usuario"),sg.Input(key="usuario", size=(10,1))],
-	[sg.Text("Senha"),
-	sg.Input(key="senha",password_char='*', size=(10,1))],
-	[sg.Checkbox("salvar login?")],
-	[sg.Button("Entar")]
-
+	[sg.Text('Usuario')],
+	[sg.Input(key='user', size=(10,1))],
+	[sg.Text('Senha')],
+	[sg.Input(key='passe', size=(10,1))],
+	[sg.Button('Login')],
+	[sg.Text(' ', key='mensagem')],
 ]
-#criando a tela
-tela=sg.Window("Login", layout)
-
-#loopo 
+#criando a janela
+window= sg.Window('Login' ,layout=layout)
+#loop para a janela ficar sempre ativa
 while True:
-	event,values=tela.read()
-	#se caso o usuario queira sair a pagina irá sair sem erro
+	event,values=window.read()
+	#sevo usuario fechar atela ,a tela irá fechar sem erro
 	if event==sg.WIN_CLOSED:
 		break
+		
+	#como não está conectado ao banco de dados,essa condição se caso o login e senha for diferente dos valores definidos nas variaveis user_c e senha_c
+	elif event=='Login':
+		senha_c='123'
+		user_c='orlando'
+		usuario1=values['user']
+		pin=values['passe']
+		if usuario1==user_c and pin==senha_c:
+			window['mensagem'].update('login feito com sucesso')
+		else:
+			window['mensagem'].update('login ou senha invalido')
+
+	
