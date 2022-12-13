eb8cc127f581e12bb86c192df944430af5b99a3c
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-12-13 08:53:13 -0300

    Update multiplasPage.py

--- Interface/PySimpleGUI/multiplasPage.py
+++ Interface/PySimpleGUI/multiplasPage.py
@@ -1,76 +1,81 @@
 import PySimpleGUI as sg
+
+
 def tela_inicial():
-		sg.theme("DarkRed")
-		layout=[
-			[sg.Text("Seja Bem-Vindo",size=(15,0))],
-			[sg.Button("Cadastro",key="cadas",   size=(30,3))],
-			[sg.Button("Informações",key="lg",size=(30,3))],
-			[sg.Button("Contatos",key="cont",size=(30,3))],
-			[sg.Button("Redes sociais",key="redes",size=(30,4))]
-		
-		]
-		return sg.Window("principal",layout=layout,finalize=True,size=(600,800))
-		
+    sg.theme("DarkRed")
+    layout = [
+        [sg.Text("Seja Bem-Vindo", size=(15, 0))],
+        [sg.Button("Cadastro", key="cadas", size=(30, 3))],
+        [sg.Button("Informações", key="lg", size=(30, 3))],
+        [sg.Button("Contatos", key="cont", size=(30, 3))],
+        [sg.Button("Redes sociais", key="redes", size=(30, 4))]
+
+    ]
+    return sg.Window("principal", layout=layout, finalize=True, size=(600, 800))
+
+
 def tela_cadastro():
-		sg.theme("DarkBlack")
-		layout=[
-				[sg.Text("Nome"),sg.Input(key="nome_1",size=(10,0))],
-				[sg.Text("Idade"),sg.Input(key="idade_1",size=(10,0))],
-				[sg.Text("CPF"),sg.Input(key="cpf",size=(10,0))],
-				[sg.Text("Telefone"),sg.Input(key="tel",size=(10,0))],
-				[sg.Text("Endereço"),sg.Input(key="ende",size=(10,0))],
-				
-				[sg.Button("Enviar")],
-				[sg.Output(size=(30,10))],
-				
-				[sg.Button("Voltar")]
-		]
-		return sg.Window("Cadastro2",layout=layout,finalize=True)
-		
+    sg.theme("DarkBlack")
+    layout = [
+        [sg.Text("Nome"), sg.Input(key="nome_1", size=(10, 0))],
+        [sg.Text("Idade"), sg.Input(key="idade_1", size=(10, 0))],
+        [sg.Text("CPF"), sg.Input(key="cpf", size=(10, 0))],
+        [sg.Text("Telefone"), sg.Input(key="tel", size=(10, 0))],
+        [sg.Text("Endereço"), sg.Input(key="ende", size=(10, 0))],
+
+        [sg.Button("Enviar")],
+        [sg.Output(size=(30, 10))],
+
+        [sg.Button("Voltar")]
+    ]
+    return sg.Window("Cadastro2", layout=layout, finalize=True)
+
+
 def tela_info():
-		layout=[
-						[sg.Radio("Cidade","cartoes",key="ci"),sg.Radio("Informacao","cartoes",key="inf")],
-				[sg.Radio("Pontos Históricos","ph",key="pont_h")],
-				
-				[sg.Button("Voltar"),sg.Button("Confirmar")]
-		]
-		return sg.Window("informacao1",layout=layout,finalize=True)
-		
-		
+    layout = [
+        [sg.Radio("Cidade", "cartoes", key="ci"), sg.Radio("Informacao", "cartoes", key="inf")],
+        [sg.Radio("Pontos Históricos", "ph", key="pont_h")],
 
-		
+        [sg.Button("Voltar"), sg.Button("Confirmar")]
+    ]
+    return sg.Window("informacao1", layout=layout, finalize=True)
 
-		
-		
-jan_inicial,janela_cadastro, jan_info=tela_inicial(),None,None
-		
+
+jan_inicial, janela_cadastro, jan_info = tela_inicial(), None, None
+
 while True:
-		window, event, values = sg.read_all_windows()
-		#Se o usuario querer sair da pagina principal sem erro
-		if window==tela_inicial and event==WIN_CLOSED :
-			break
-			
-			
-		##Eventos tela de Cadastro
-		if window==tela_cadastro and event==WIN_CLOSED:
-				break
-				
-		if window==jan_inicial and event=="cadas":
-			janela_cadastro=tela_cadastro()
-			jan_inicial.hide()
-			
-		if window==janela_cadastro and event=="Voltar":
-		          janela_cadastro.hide()
-		          jan_inicial.un_hide()
+    window, event, values = sg.read_all_windows()
+    # Se o usuario querer sair da pagina principal sem erro
+    if window == jan_inicial and event == sg.WIN_CLOSED:
+        break
 
-		#Eventos Tela de Informações
-		if window==jan_info and event==WIN_CLOSED:
-					break
-			
-		if window==jan_inicial and event=="lg":
-			jan_info=tela_info()
-			jan_inicial.hide()
-			
-		if window==jan_info and event=="Voltar":
-			jan_info.hide()
-			jan_inicial.un_hide()
+    ##Eventos tela de Cadastro
+    if window == janela_cadastro and event == sg.WIN_CLOSED:
+        break
+
+    if window == jan_inicial and event == "cadas":
+        janela_cadastro = tela_cadastro()
+        jan_inicial.hide()
+
+    if window == janela_cadastro and event == "Voltar":
+        janela_cadastro.hide()
+        jan_inicial.un_hide()
+
+    # Eventos Tela de Informações
+    if window == jan_info and event ==sg.WIN_CLOSED:
+        break
+
+    if window == jan_inicial and event == "lg":
+        jan_info = tela_info()
+        jan_inicial.hide()
+
+    if window == jan_info and event == "Voltar":
+        jan_info.hide()
+        jan_inicial.un_hide()
+
+    if window==jan_info and event=="Confirmar":
+        if values["ci"]==True:
+            sg.popup("A capital do nordeste")
+
+        elif values["inf"]==True:
+            sg.popup("A cidade tem ...anos,é conhecida por sua cultura,pelo seu clima e seus pontos turisticos são os mais visitados do Brasil")
