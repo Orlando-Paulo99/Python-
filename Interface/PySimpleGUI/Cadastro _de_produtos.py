4b156d722605e222558833408b146f57f399cbc6
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-12-20 19:16:44 -0300

    Update Cadastro _de_produtos.py

--- Interface/PySimpleGUI/Cadastro _de_produtos.py
+++ Interface/PySimpleGUI/Cadastro _de_produtos.py
@@ -2,13 +2,14 @@ import PySimpleGUI as sg
 
 #CRIANDO O FORMATO DA TELA DE LOGIN
 def tela_login():
-	sg.theme("DarkBlue4")
+	sg.theme("LightBrown10")
 	layout=[
 			[sg.Text("Nome")],
 			[sg.Input(key="user",size=(15,1))],
 			[sg.Text("ID")],
 			[sg.Input(key="ident",size=(15,1))],
-			[sg.Button("Entrar")]
+			[sg.Button("Entrar")],
+			[sg.Text(' ',key="mensagem")]
 	]
 	return sg.Window("login",layout=layout,finalize=True)
 
@@ -42,10 +43,7 @@ def verificar_produtos():
 			[sg.Button("Voltar")]
 	]
 	return sg.Window("verificando",layout=layout,finalize=True)
-	
-	
-	
-	
+		
 #CRIANDO JANELAS
 jan_login,jan_option,jan_cadastro,jan_verificar=tela_login(),None,None,None
 
@@ -55,11 +53,19 @@ while True:
 	#EVENTO TELA LOGIN PARA ENCERRAR O PROGRAMA
 	if window==jan_login and event==sg.WIN_CLOSED:
 		break
-	#EVENTO  PARA IR PARA JANELA DE OPÇÕES
-	if window==jan_login and event=="Entrar":
-		jan_option=tela_option()
-		jan_login.hide()
 		
+	#EVENTO  PARA IR PARA JANELA DE OPÇÕES
+	#EVENTO TELA DE LOGIN
+	if window==jan_login and event=="Entrar":
+		user_1=values["user"]
+		pin=values["ident"]
+		user_2="orlando"
+		pin_2="123"
+		if user_1==user_2 and pin==pin_2:
+			jan_option=tela_option()
+			jan_login.hide()
+		else:
+			window["mensagem"].update("login ou senha invalido")
 	#EVENTO PARA FECHAR A TELA DE OPÇÕES
 	if  window==jan_option and event==sg.WIN_CLOSED:
 		break
@@ -68,6 +74,12 @@ while True:
 	if window==jan_option and event=="Cadastrar Produto":
 		jan_cadastro=tela_cadastroProdutos()
 		jan_option.hide()
+		
+	#VOLTANDO PARA TELA DE LOGIN
+	if window==jan_option and event=="Desconectar":
+		jan_login=tela_login()
+		jan_option.hide()
+		jan_login.un_hide()
 		
 	#VOLTANDO PARA TELA DE OPÇÕES
 	if window==jan_cadastro and event=="Voltar":
@@ -78,29 +90,12 @@ while True:
 	#EVENTO PARA FECHAR A TELA DE PRODUTOS
 	if window==jan_cadastro and event==sg.WIN_CLOSED:
 		break
-
-	'''if window==jan_cadastro:
-		if event=="Confirmar":
-			 	nome_p=values["nome_produto"]
-			 	valor_p=values["valor_produto"]
-			 	jan_verificar["saida"].update(f"Nome do produto:{nome_p} \nvalor:{valor_p}")'''
-		 	
+	 	
 	if window==jan_cadastro and event=="Confirmar":
 			varif=tela_cadastroProdutos()
 			
 			window["saida"].update("")
 			verif.get_tela_cadastroProdutos()
-			
-			
-			
-			
-			
-		
-		
-	
-	
-	
-	
 	
 	#EVENTOS VERIFICAR PRODUTOS
 	if window==jan_option and event=="Verificar Produtos":
@@ -113,4 +108,3 @@ while True:
 		jan_option.un_hide()
 
 
-		
